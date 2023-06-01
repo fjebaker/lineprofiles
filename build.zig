6fc8be19be2950345b7d358d2ede356960251543
fjebaker <fergusbkr@gmail.com>
2023-06-01 16:10:47 +0100

    chore: cleanup build file

--- build.zig
+++ build.zig
@@ -6,39 +6,30 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const zigfitsio = zigFITSTIO.create(b, target);
-    const build_xspec = b.option(bool, "xspec", "Build XSPEC shared library.") orelse false;
-    if (build_xspec) {
-        const xspec = b.addStaticLibrary(.{
-            .name = "xslineprofiles",
-            .root_source_file = .{ .path = "src/xspec-wrapper.zig" },
-            .target = target,
-            .optimize = optimize,
-        });
-        zigfitsio.link(xspec);
-        b.installArtifact(xspec);
-    } else {
-        const xspec = b.addSharedLibrary(.{
-            .name = "xslineprofiles",
-            .root_source_file = .{ .path = "src/xspec-wrapper.zig" },
-            .target = target,
-            .optimize = optimize,
-            .version = .{ .major = 0, .minor = 1 },
-        });
-        zigfitsio.link(xspec);
-        b.installArtifact(xspec);
-    }
 
-    var main_tests = b.addExecutable(.{
+    const xspec = b.addStaticLibrary(.{
+        .name = "xslineprofiles",
+        .root_source_file = .{ .path = "src/xspec-wrapper.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    zigfitsio.link(xspec);
+
+    const xspec_step = b.step("xspec", "Build the XSPEC static library");
+    const install_xspec = b.addInstallArtifact(xspec);
+    xspec_step.dependOn(&install_xspec.step);
+
+    var plots = b.addExecutable(.{
         .name = "plots",
         .root_source_file = .{ .path = "src/test.zig" },
         .target = target,
         .optimize = optimize,
     });
-    zigfitsio.link(main_tests);
+    zigfitsio.link(plots);
 
-    const test_step = b.step("plots", "Create debugging plots");
-    const run_step = b.addRunArtifact(main_tests);
-    const ins_step = b.addInstallArtifact(main_tests);
-    test_step.dependOn(&ins_step.step);
-    test_step.dependOn(&run_step.step);
+    const plots_step = b.step("plots", "Create debugging plots");
+    const install_plots = b.addInstallArtifact(plots);
+    const run_plots = b.addRunArtifact(plots);
+    plots_step.dependOn(&install_plots.step);
+    plots_step.dependOn(&run_plots.step);
 }
